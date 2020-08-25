86e13bc4e847079c4d4a3fa6993b9d62eca9bd7f
Andres <an.cadena@udla.edu.co>
2020-08-24 22:31:42 -0500

    evaluacion1

--- src/public/javascript/Dashboard/respuestas.js
+++ src/public/javascript/Dashboard/respuestas.js
@@ -1,10 +1,44 @@
-function validar() {
-    console.log("entro respuestas");
-    var x = document.getElementsByClassName("prueba");
-    for (let index = 0; index < x.length; index++) {
-    console.log(x[index].checked);
+
+
+function resultado() {
+    console.log("hola res");
+    var p1, p2, p3, p4,p5, p6, p7, p8,p9, p10, nota;
+
+    if (document.getElementById('f1').checked == true) { p1 = 0.5 }
+    else { p1 = 0 }
+
+    if (document.getElementById('v2').checked == true) { p2 = 0.5 }
+    else { p2 = 0 }
+
+    if (document.getElementById('f3').checked == true) { p3 = 0.5 }
+    else { p3 = 0 }
+
+    if (document.getElementById('v4').checked == true) { p4 = 0.5 }
+    else { p4 = 0 }
+
+    if (document.getElementById('v5').checked == true) { p5 = 0.5 }
+    else { p5 = 0 }
     
-}
-console.log(x[0]);
+    if (document.getElementById('f6').checked == true) { p6 = 0.5 }
+    else { p6 = 0 }
+
+    if (document.getElementById('v7').checked == true) { p7 = 0.5 }
+    else { p7 = 0 }
+
+    if (document.getElementById('v8').checked == true) { p8 = 0.5 }
+    else { p8 = 0 }
+
+    if (document.getElementById('v9').checked == true) { p9 = 0.5 }
+    else { p9 = 0 }
+
+    if (document.getElementById('f10').checked == true) { p10 = 0.5 }
+    else { p10 = 0 }
+
+    nota = p1 + p2 + p3 + p4+ p5 + p6 + p7 + p8 + p9 + p10;
+    alert(" Aciertos: " + nota);
+    location.reload();
+    return nota;
 }
 
+const nota= resultado;
+module.exports = nota;
