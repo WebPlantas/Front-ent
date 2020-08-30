1511bb998a56746c1c900fd6397248fc80f71b50
zambrano-22 <zambrano20407@gmail.com>
2020-08-30 06:26:46 -0500

    modulo contenido

--- src/routes/adminProfesor.js
+++ src/routes/adminProfesor.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 
-router.get('/crearclase', (req, res, next) => {
+router.get('/adminprofesor', (req, res, next) => {
     res.render('Dashboard/Profesor/Clase/clase', {
+        layout: 'profesor'
+    });
+
+});
+
+router.get('/crearcontenido', (req, res, next) => {
+    res.render('Dashboard/Profesor/ContenidoP/newContenido', {
         layout: 'profesor'
     });
 
