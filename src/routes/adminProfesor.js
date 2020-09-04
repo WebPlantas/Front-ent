28485b160b664b0594dd3919b31ecf30ae9b4e71
zambrano-22 <zambrano20407@gmail.com>
2020-09-03 21:26:51 -0500

    finished teacher profile

--- src/routes/adminProfesor.js
+++ src/routes/adminProfesor.js
@@ -1,14 +1,11 @@
 const router = require('express').Router();
 const {
-    GetPerfil
-  } = require('./../services/adminProfesor');
+    GetPerfil,
+    UpdateProfesorAdmin,
+    UpdateUserProfesorAdmin
+} = require('./../services/adminProfesor');
 
-// router.get('/adminprofesor', (req, res, next) => {
-//     res.render('Dashboard/Profesor/Clase/clase', {
-//         layout: 'profesor'
-//     });
-
-// });
+router.get('/perfilprofesor', GetPerfil);
 
 router.get('/crearcontenido', (req, res, next) => {
     res.render('Dashboard/Profesor/ContenidoP/newContenido', {
@@ -24,13 +21,9 @@ router.get('/detalleclase', (req, res, next) => {
 
 });
 
-router.get('/perfilprofesor', GetPerfil);
+router.post('/UpdateProfesorAdmin', UpdateProfesorAdmin);
 
-// router.get('/perfilprofesor', (req, res, next) => {
-//     res.render('Dashboard/Profesor/Perfil/perfil', {
-//         layout: 'profesor'
-//     });
+router.post('/UpdateUserProfesorAdmin', UpdateUserProfesorAdmin)
 
-// });
 
 module.exports = router;
--- src/services/adminProfesor.js
+++ src/services/adminProfesor.js
@@ -1,22 +1,37 @@
-const {
-    pool
-  } = require('../config/connection');
+const {pool} = require('../config/connection');
+const helpers = require('../util/lib/helpers');
 
-  var perfilActual;
-  const GetPerfil = async (req, res, next) => {
-    perfilActual = req.params.Id;
-    console.log('Get');
-    await pool.query(
-      `
-      SELECT  
+var perfilActual;
+const GetPerfil = async (req, res, next) => {
+  perfilActual = req.params.Id;
+  console.log('Get');
+  await pool.query(
+    `
+      SELECT
+      Persona.idPersona AS Id,
+      Persona.NumeroIdentificacion AS Identificacion,
+      Persona.Nombre,
+      Persona.Apellidos,
+      TipoDocumento.tipo AS Tipo,
+      TipoDocumento.idTipoDocumento,
       CONCAT( Nombre, ' ', Apellidos) AS Nombres,
+      Direccion,
+      Telefono.Telefono,
+      CONCAT(Persona.FechaNacimiento, '') AS FechaNacimiento,
+      Usuario.Username AS User,  
       rol.rol AS Rol,
+      rol.idRol,
+      Genero.idGenero,
       Genero.genero AS Genero,
       email.email AS Email,
       telefono.Telefono AS Telefono,
       Persona.EstadoPersona AS Estado 
 
-    FROM Persona INNER JOIN usuario
+    FROM Persona INNER JOIN TipoDocumento
+      ON
+      TipoDocumento.idTipoDocumento = Persona.TipoDocumento_idTipoDocumento
+      INNER JOIN 
+      usuario
       ON
       usuario.Persona_idPersona = Persona.idPersona
       INNER JOIN
@@ -36,7 +51,7 @@ const {
       ON
       telefono.Persona_idPersona = persona.idPersona
       WHERE 
-      Persona.EstadoPersona = 'Activo';
+      Usuario.idUsuario = ${req.user.idUsuario};
       `, (err, data) => {
       console.log(data);
       if (!err && data.length > 0) {
@@ -51,9 +66,109 @@ const {
         });
       }
     }
-    );
-  };
-  
-  module.exports = {
-    GetPerfil,
-  };
+  );
+};
+
+const UpdateProfesorAdmin = async (req, res, next) => {
+  console.log("ENTRO POS", req.body);
+  await pool.query(
+    `
+        SELECT 
+        Persona_idPersona AS ID
+        FROM 
+          Profesor
+        WHERE
+        Persona_idPersona = '${req.user.Persona_idPersona}'
+      `,
+    async (er, dt) => {
+      console.log("und", dt[0]);
+      if (!er && dt.length > 0) {
+        console.log(req.body.genero);
+        await pool.query(
+          `
+              UPDATE
+                Persona
+              INNER JOIN 
+                Genero
+              ON
+                Genero.idGenero = Persona.Genero_idGenero
+              INNER JOIN 
+                Telefono
+              ON
+                Telefono.Persona_idPersona = Persona.idPersona
+              SET
+              NumeroIdentificacion = ${req.body.document},
+              Nombre = '${req.body.firstname}',
+              Apellidos = '${req.body.lastname}',
+              Direccion = '${req.body.direccion}',
+              FechaNacimiento = '${req.body.fnacimiento}',
+              Persona.Genero_idGenero = '${req.body.genero}',
+              Telefono.Telefono = '${req.body.phone}'
+              WHERE 
+              idPersona = ${dt[0].ID}
+            `,
+          async (err, data) => {
+            console.log("data pos estu", err);
+            if (!err && data.affectedRows > 0) {
+              res.redirect(`/perfilprofesor`);
+            } else {
+              res.send(`error`, err);
+            }
+          }
+        );
+      } else {
+        res.redirect(`/perfilprofesor`);
+      }
+    }
+  );
+};
+
+const UpdateUserProfesorAdmin = async (req, res, next) => {
+  var password = req.body.password;
+  if (password == req.body.password1) {
+    var validPassword = await helpers.encrytPassword(password);
+    await pool.query(
+      `
+          SELECT * FROM Usuario 
+          WHERE username = '${req.body.username}'      
+          `, async (error, users) => {
+        console.log("users", users);
+        if (!error && users.length <= 0) {
+          await pool.query(
+            `
+                        UPDATE
+                          Usuario
+                        INNER JOIN
+                          Email
+                        ON
+                          Email.idEmail = Usuario.Email_idEmail
+                        SET
+                          Username = '${req.body.username}',
+                          Password = '${validPassword}'
+                        WHERE 
+                        idUsuario = ${req.user.idUsuario}
+                      `,
+            async (err, data) => {
+              console.log("data pos ", err);
+              if (!err && data.affectedRows > 0) {
+                res.redirect(`/perfilprofesor`);
+              } else {
+                res.send(`error`, err);
+              }
+            }
+          );
+        } else {
+          res.redirect(`/perfilprofesor`);
+        }
+      }
+    )
+  } else {
+    res.send("error las contraseñas no coinciden")
+  }
+};
+
+module.exports = {
+  GetPerfil,
+  UpdateProfesorAdmin,
+  UpdateUserProfesorAdmin
+};
