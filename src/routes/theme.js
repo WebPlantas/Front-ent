c8b50b8a892fb7cc6b9c1537b2a38044ac4b7de7
zambrano-22 <zambrano20407@gmail.com>
2020-09-08 07:46:21 -0500

    modulo partes de la celula terminado

--- src/routes/theme.js
+++ src/routes/theme.js
@@ -15,8 +15,25 @@ router.get('/celula', (req, res, next) => {
 router.get('/historia', (req, res, next) => {
     res.render('Dashboard/Tematicas/Historia', {});
 });
+
 router.get('/definicion', (req, res, next) => {
     res.render('Dashboard/Tematicas/Definicion', {});
+});
+
+router.get('/partes', (req, res, next) => {
+    res.render('Dashboard/Tematicas/Partes', {});
+});
+
+router.get('/nucleo', (req, res, next) => {
+    res.render('Dashboard/Tematicas/Nucleo', {});
+});
+
+router.get('/citoplasma', (req, res, next) => {
+    res.render('Dashboard/Tematicas/Citoplasma', {});
+});
+
+router.get('/membrana', (req, res, next) => {
+    res.render('Dashboard/Tematicas/membranaCelular', {});
 });
 
 module.exports = router;
