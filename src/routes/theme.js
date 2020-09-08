912dd20d26d8362515ed5c1f2517db9775cecb31
zambrano-22 <zambrano20407@gmail.com>
2020-09-08 04:54:01 -0500

    modulo celula terminado

--- src/routes/theme.js
+++ src/routes/theme.js
@@ -2,17 +2,21 @@ const router = require('express').Router();
 
 router.get('/tematica', (req, res, next) => {
     res.render('Dashboard/Tematicas/Tematica', {});
-
 });
 
 router.get('/contenido', (req, res, next) => {
-    res.render('Dashboard/Tematicas/Contenido', {});
+    res.render('Dashboard/Tematicas/modulos', {});
+});
 
+router.get('/celula', (req, res, next) => {
+    res.render('Dashboard/Tematicas/laCelula', {});
 });
 
 router.get('/historia', (req, res, next) => {
     res.render('Dashboard/Tematicas/Historia', {});
-
+});
+router.get('/definicion', (req, res, next) => {
+    res.render('Dashboard/Tematicas/Definicion', {});
 });
 
 module.exports = router;
